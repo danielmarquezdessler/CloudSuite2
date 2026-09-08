13a63f59cefaa1922bfa3a10f9ecde1bad9d4141
juliomarquez-Montreal <julio.marquez@montreal.com.br>
2026-09-08 12:14:40 -0300

    Complete initial CloudSuite branding

--- web/src/Layout/Footer.tsx
+++ web/src/Layout/Footer.tsx
@@ -1,5 +1,4 @@
 import { Col, Row } from "react-bootstrap";
-import {Link} from "react-router-dom";
 
 
 const Footer = () => {
@@ -8,16 +7,10 @@ const Footer = () => {
             <div className="footer-wrapper container-fluid">
                 <Row>
                     <Col sm={6} className="my-1">
-                        <p className="m-0">Made with &#9829; by Team <Link to="https://themeforest.net/user/phoenixcoded"
-                            target="_blank">Phoenixcoded</Link></p>
+                        <p className="m-0">CloudSuite 2 · Gestión de campañas políticas</p>
                     </Col>
                     <Col sm={6} className="ms-auto my-1">
-                        <ul className="list-inline footer-link mb-0 justify-content-sm-end d-flex">
-                            <li className="list-inline-item"><Link to="/">Home</Link></li>
-                            <li className="list-inline-item"><Link to="https://pcoded.gitbook.io/light-able/"
-                                target="_blank">Documentation</Link></li>
-                            <li className="list-inline-item"><Link to="https://phoenixcoded.support-hub.io/" target="_blank">Support</Link></li>
-                        </ul>
+                        <p className="mb-0 text-sm text-sm-end">Plataforma en preparación</p>
                     </Col>
                 </Row>
             </div>
